4d9beccc9f2c5c6fec5fb88d954ec04ca2159ec0
Abhisek Chowdhury <abhisek12.yuvraj@gmail.com>
2018-10-03 18:44:19 +0530

    youtube import handler updated

--- import-handlers/youtube.php
+++ import-handlers/youtube.php
@@ -5,38 +5,49 @@ $project_path = dirname(__DIR__);
 // import libraries
 require_once $project_path . '/classes/utilities-common.php';
 
+// setting up the data source
+$src_id = "youtube";
+$data_src = $project_path . "/data/AMNHorg";
 
+// remove existing data
+ $source_details = new DataSource($src_id);
+ $source_details->remove_all_douments();
+ echo 'Deleted '.PHP_EOL;
+// exit(0);
 
 
 // define source specific class
 class YoutubeImportHandler extends ImportHandler
 {
-
-    private $coll_id = null;
-
-    private $coll_name = null;
     
     public static $id_arr = array();
 
-    public function process_src_document($src_document)
-    {
+    public function process_src_document($src_document){
         // print_r(($src_document)); //exit(0);
         $youtube_id = null;
         
-        if(isset($src_document->snippet->playlistId)){
-            $youtube_id .= $src_document->snippet->playlistId;
-        } 
+//         if(isset($src_document->snippet->playlistId)){
+//             $youtube_id .= $src_document->snippet->playlistId;
+//         }
+
+        
         if (isset($src_document->id->videoId)) {
-            $youtube_id .= $src_document->id->videoId;
+        
+            $youtube_id = $src_document->id->videoId;
+        
         } else if (isset($src_document->contentDetails)) {
-            $youtube_id .= $src_document->contentDetails->videoId;
+            
+            $youtube_id = $src_document->contentDetails->videoId;
+        
         }
                
         
         
         if ($youtube_id == null) {
-            print_r("Youtube ID issue. Ignored : " . $src_document->etag . PHP_EOL);
-        } else {
+            print_r("Youtube ID is NULL. Ignored : " . $src_document->etag . PHP_EOL);
+        } 
+        
+        else {
             
             $ndli_doc_id = $this->generate_ndli_document_id($youtube_id);
             $document = new NDLIDocument($ndli_doc_id);
@@ -65,23 +76,13 @@ class YoutubeImportHandler extends ImportHandler
         }
     }
 
-    public function process_all_documents($data_src)
-    {
+    public function process_all_documents($data_src){
         $playlists = FileSystemUtils::get_filelist($data_src);
-        //$structure = new NDLIStructure($this->src_id);
 
         foreach ($playlists as $playlist) {
-
             $data = json_decode(file_get_contents($data_src . "/" . $playlist));
             print_r($data_src . "/" . $playlist . PHP_EOL);
             // exit(0);
-
-            //if (isset($data->items[0]->snippet->playlistId)) {
-                //$this->col_id = $data->items[0]->snippet->playlistId;
-                //$this->col_name = ucwords($playlist);
-                //$structure->add_collection($this->col_id, $this->col_name);
-           //}
-
             foreach ($data->items as $src_document) {
                 // print_r($src_document);
                 $this->process_src_document($src_document);
@@ -92,24 +93,19 @@ class YoutubeImportHandler extends ImportHandler
         $source_details = new DataSource($this->src_id);
         $source_details->store_documents($this->get_documents()); // store a set of data
         $source_details->index_all_documents(); // commit
-        //$source_details->set_structure($structure);
+
     }
 
-    protected function generate_ndli_document_id(string $youtube_id)
-    {
+    protected function generate_ndli_document_id(string $youtube_id){
         return preg_replace("/(\W+)/", "_", $youtube_id);
     }
 }
+
+
+
+
 // ////////////////////////////////////////////////////////////////////////////////////////////////
 
-// setting up the data source
-$src_id = "youtube";
-$data_src = $project_path . "/data/AMNHorg";
-
-// remove existing data
-// $source_details = new DataSource($src_id);
-// $source_details->remove_all_douments();
-// echo 'Deleted '.PHP_EOL; exit(0);
 
 // processing
 $youtube_import_handler = new YoutubeImportHandler($src_id);
